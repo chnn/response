60f9b31b0304e76ef2214fa389a412a089425c2c
Chris Henn <chris@chrishenn.net>
2012-08-17 21:05:10 -0700

    Use dictionary for `responder: message` store

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+from itertools import izip
 from flask import Flask, render_template, request, redirect
 import twilio.twiml
 
@@ -6,24 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', responders=responders, messages=messages)
+    return render_template('index.html', responders=responders)
 
-responders = []
-messages = []
+responders = {}
 
 @app.route('/message', methods=['GET', 'POST'])
 def receive_text_message():
     """Receive Twilio text message with caller ID"""
 
     responder = request.values.get('From', None)
-    responders.append(responder)
     message = request.values.get('Body', None)
-    messages.append(message)
+
+    responders[responder] = message
 
     resp = twilio.twiml.Response()
     resp.sms('Verified that ' + responder + ' responded with "' + message + '"')
     return str(resp)
 
+def phone_to_name(phonenumber):
+    """Return a name given a phone number, if exists in db"""
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
