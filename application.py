c5760e67f2bebaf6f7809418e179e7d416403934
Chris Henn <chris@chrishenn.net>
2012-08-17 18:08:15 -0700

    Test out Twilio feature

--- application.py
+++ application.py
@@ -9,14 +9,18 @@ def index():
     return render_template('index.html')
 
 responders = {
+        "+14154976513": "Chris Henn"
 }
 
 @app.route('/message', methods=['GET', 'POST'])
 def receive_text_message():
     """Receive Twilio text message with caller ID"""
 
+    responder = request.values.get('From', None)
+    message = "Verified: " + responders[responder] + " is responding."
+
     resp = twilio.twiml.Response()
-    resp.sms("Hello")
+    resp.sms(message)
     return str(resp)
     
 
