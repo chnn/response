726ecf5d0d5d68446ea9bbfbe0a1f5abcce1ea7e
Chris Henn <chris@chrishenn.net>
2013-01-07 13:41:12 -0800

    Remove Pusher api keys, update README

--- README.markdown
+++ README.markdown
@@ -1,8 +1,14 @@
-Response is a web app for handling the response to an incident (similar to iamresponding.com). The goal is to make it very simple and effective to use.
+Response is a prototype web app for coordinating emergency responders to incident.
 
-Response is incomplete, untested, and in no way ready for production.
+<a href="./response_screen.png"><img src="./response_screen_thumb.png" /></a>
+
+Text your response to a Twilio phone number, and it will be pushed to the list in the web app. The list can be sorted edited to give the incident coordinator a quick overview of who is available. Data is persisted to a Mongo data store. 
+
+This prototype was born out of frustration with current solutions to the same problem (iamresponding.com). It also provided an opportunity to explore the Twilio and Pusher APIs, and implement a Backbone.js based interface.
 
 ### Technologies
+
+The code is simple, but there are quite a few interactions between technologies that make the whole thing work.
 
 * Flask 
     * Serves the index.html template and static assets (backbone, styles)
@@ -15,6 +21,6 @@ Response is incomplete, untested, and in no way ready for production.
     * Renders the important parts of the application without page refresh
     * Seperates JS into more maintainable parts
 
-### What's planned 
+### Possible improvements to be made
 
-Check out the [development board on Trello](https://trello.com/b/RmoQCeN3)
+There is a [development board on Trello](https://trello.com/b/RmoQCeN3) with some ideas for further improving the app.
--- application.py
+++ application.py
@@ -1,41 +1,38 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, Response, jsonify
+from flask import Flask, render_template, request, Response, json
 from mongokit import Connection, Document
-from datetime import datetime
-import json
 import twilio.twiml
 import pusher
 
-# Mongo setup 
+# Mongo setup
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 
 # Pusher setup
-pusher.app_id = '26515'
-pusher.key = 'fcae1137cc539c41993f'
-pusher.secret = '53ac4bf1196b576c3ec1'
+pusher.app_id = ''
+pusher.key = ''
+pusher.secret = ''
 p = pusher.Pusher()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 connection = Connection(app.config['MONGODB_HOST'],
-                app.config['MONGODB_PORT'])
+                        app.config['MONGODB_PORT'])
+
 
 # Mongo document schema for a response
 @connection.register
 class Message(Document):
+    def __repr__(self):
+        return '<Message from %r>' % (self.name)
     __collection__ = 'responses'
     __database__ = 'dev'
     structure = {
-            'name': str,
-            'message': unicode
-    }
-    validators = {
+        'name': str,
+        'message': unicode
     }
     use_dot_notation = True
-    def __repr__(self):
-        return '<Message from %r>' % (self.name)
 
 # Will eventually be a better data store (mongo)
 people = {
@@ -44,11 +41,13 @@ people = {
 
 responders = {}
 
+
 @app.route('/')
 def index():
     return render_template('index.html', responders=responders)
 
-@app.route('/message', methods=['GET', 'POST'])
+
+@app.route('/message', methods=['POST'])
 def receive_text_message():
     """Receive Twilio text message with caller ID"""
 
@@ -56,7 +55,10 @@ def receive_text_message():
     message_content = request.values.get('Body', None)
 
     # Send response to Pusher (for Backbone)
-    p['responses'].trigger('textresponse', {'from': responder, 'message': message_content})
+    p['responses'].trigger('textresponse', {
+        'from': responder,
+        'message': message_content
+    })
 
     # Save to Database
     response = connection.Message()
@@ -64,12 +66,13 @@ def receive_text_message():
     response.message = message_content
     response.save()
 
-    # # Reply to text, verifying response received
-    # resp = twilio.twiml.Response()
-    # resp.sms('Verified that ' + responder + ' responded with "' + message_content + '"')
-    # return str(resp)
+    # Reply to text, verifying response received
+    resp = twilio.twiml.Response()
+    resp.sms('Verified that ' + responder + ' responded with "' + message_content + '"')
+    return str(resp)
 
-@app.route('/responses', methods = ['GET'])
+
+@app.route('/responses', methods=['GET'])
 def return_responses():
     responses = []
     i = 0
@@ -87,15 +90,6 @@ def return_responses():
     resp = Response(data, status=200, mimetype='application/json')
     return resp
 
-@app.route('/addtestmessages')
-def push_test_messages():
-    p['responses'].trigger('textresponse', {'from': 'Chris Henn', 'message': 'Testing messages'})
-    p['responses'].trigger('textresponse', {'from': 'A Person', 'message': 'Hello'})
-    p['responses'].trigger('textresponse', {'from': 'Someone Else', 'message': 'Goodbye'})
-    p['responses'].trigger('textresponse', {'from': 'Test Person', 'message': 'Whats up'})
-    p['responses'].trigger('textresponse', {'from': 'Name Lastname', 'message': 'This is my message'})
-    p['responses'].trigger('textresponse', {'from': 'Someones Name', 'message': 'Hi'})
-    return "Success"
 
 def phone_to_name(phonenumber):
     """Return a name given a phone number, if exists in db"""
